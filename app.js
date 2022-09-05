081f53b95fdd071273abfcae02216283dd8c8720
soriagorgoroso <112647797+soriacencosud@users.noreply.github.com>
2022-09-05 14:50:37 -0300

    servidor corriendo

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+const PORT = 3030
+
+
+app.get('/', (req,res)=>{
+
+    app.render(__dirname + 'index.html', {
+    })
+
+})
+
+
+app.listen(PORT, (req,res)=> {
+
+    console.log(`Servidor escuchando en el puerto ${PORT}`)
+
+})
