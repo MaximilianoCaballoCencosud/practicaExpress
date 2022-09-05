4f403842daf6531d22395dd35950b27ef4396777
jorgedasilvabermudez <jorge.dasilvabermudez@cencosud.com>
2022-09-05 17:38:14 -0300

    fix home ruta

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 const PORT = 3030
 
 app.use(express.static('public'))
 
 app.get('/', (req,res)=>{
- 
-    res.sendFile(__dirname + '/DH-Heroes/views/index.html', {
-    })
 
-})
+    res.sendFile( path.join(__dirname, '/DH-Heroes/views/home.html'));
+    });
 
 
 app.listen(PORT, (req,res)=> {
